45b6249999004490741dac749d4b518091c75891
Paul Tschirhart <pkt3c@dram-server2.ece.umd.edu>
2015-03-02 17:03:38 -0500

    fixed the path to the system ini file

--- ptlsim/cache/memoryController.cpp
+++ ptlsim/cache/memoryController.cpp
@@ -66,7 +66,7 @@ MemoryController::MemoryController(W8 coreid, const char *name,
 
     //TODO: make the pwd argument settable by a simconfig option or maybe even by a #define in scons? 
         DRAMSim::CSVWriter &CSVOut = DRAMSim::CSVWriter::GetCSVWriterInstance("cvs_out"); 
-	mem = DRAMSim::getMemorySystemInstance("ini/DDR3_micron_8M_8B_x16_sg15.ini", "system.ini", "../DRAMSim2", "MARSS", qemu_ram_size>>20, CSVOut); 
+	mem = DRAMSim::getMemorySystemInstance("ini/DDR3_micron_8M_8B_x16_sg15.ini", "ini/system.ini", "../DRAMSim2", "MARSS", qemu_ram_size>>20, CSVOut); 
 
 	typedef DRAMSim::Callback <Memory::MemoryController, void, uint, uint64_t, uint64_t> dramsim_callback_t;
 	DRAMSim::TransactionCompleteCB *read_cb = new dramsim_callback_t(this, &MemoryController::read_return_cb);
